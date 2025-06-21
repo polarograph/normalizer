81e6ba13780ec96ed4cf1a6fb8d2acb284691850
polarograph <polar@polarite.club>
2025-06-22 00:08:51 +0300

    fix: switch BedBlockMixin to a redirect instead of an inject
    Does the same thing & fixes a bug where you instantly step to the ground upon walking off

--- src/main/java/club/polarite/normalizer/mixin/BedBlockMixin.java
+++ src/main/java/club/polarite/normalizer/mixin/BedBlockMixin.java
@@ -3,25 +3,22 @@ package club.polarite.normalizer.mixin;
 import club.polarite.normalizer.config.ConfigManager;
 
 import net.minecraft.world.entity.Entity;
-import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.BedBlock;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
-import java.io.ObjectInputFilter;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 /**
  * Disables bed bounce.
  */
 @Mixin(BedBlock.class)
 public class BedBlockMixin {
-    @Inject(method = "updateEntityMovementAfterFallOn", at = @At("HEAD"), cancellable = true)
-    private void disableBounce(BlockGetter blockGetter, Entity entity, CallbackInfo ci) {
-        if (ConfigManager.getConfig().disableBedBounce && ConfigManager.isWhitelisted) {
-            ci.cancel();
-        }
+    @Redirect(
+            method = "updateEntityMovementAfterFallOn",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/Entity;isSuppressingBounce()Z")
+    )
+    private boolean fakeSuppressBounce(Entity entity) {
+        return (ConfigManager.getConfig().disableBedBounce && ConfigManager.isWhitelisted) || entity.isSuppressingBounce();
     }
 }
